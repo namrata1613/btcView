998e859705450fbeb23ee85c7d5f9a1e41ef19ca
namrata1613 <dasnamrata2402@gmail.com>
2023-08-19 23:47:00 +0530

    sma

--- backtest.py
+++ backtest.py
@@ -8,7 +8,7 @@ class RSIStrategy(bt.Strategy):
 
     
     def next(self) :
-
+        
         if self.rsi < 30 and not self.position:
             self.buy(size=1)
             #print("Oversold")
@@ -18,6 +18,35 @@ class RSIStrategy(bt.Strategy):
             #print("Overbought")
 
 
+class SMAStrategy(bt.Strategy) :
+
+    def __init__(self) :
+        self.short_sma = bt.talib.SMA(self.data.close,timeperiod=20)
+        self.long_sma = bt.talib.SMA(self.data.close,timeperiod=50)
+        self.flag =0
+
+    def next(self) :
+        if self.order : 
+            return 
+        
+        current_position = self.getposition().size
+
+        if current_position>0 : 
+            if self.short_sma < self.long_sma :
+                self.order = self.sell(size=1)
+                print("sell")
+
+        if current_position < 0:
+            if self.short_sma > self.long_sma and not self.position.size==0 :
+                self.order = self.buy(size=1)
+                print("buy")
+
+        print(current_position)
+        
+        
+            
+            
+        
 
 cerebro = bt.Cerebro()
 
@@ -25,7 +54,8 @@ data = bt.feeds.GenericCSVData(dataname='daily.csv',dtformat=2)
 
 cerebro.adddata(data)
 
-cerebro.addstrategy(RSIStrategy)
+#cerebro.addstrategy(RSIStrategy)
+cerebro.addstrategy(SMAStrategy)
 
 cerebro.run()
 
